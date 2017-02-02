47eaa4f35c6d79e0c66a3bb9a54d68c422283f6a
aramzham <aramzham@hotmail.com>
2017-02-02 21:10:26 +0400

    shapeArea

--- Experimenting/CodeFights Intro/Program.cs
+++ Experimenting/CodeFights Intro/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeFights_Intro
 {
@@ -16,14 +14,14 @@ namespace CodeFights_Intro
         }
         public static bool checkPalindrome(string inputString)
         {
-            return inputString.Substring(0, inputString.Length/2) ==
-                   new string(inputString.Reverse().ToArray()).Substring(0, inputString.Length/2);
+            return inputString.Substring(0, inputString.Length / 2) ==
+                   new string(inputString.Reverse().ToArray()).Substring(0, inputString.Length / 2);
         }
         public static int adjacentElementsProduct(int[] inputArray)
         {
             var list = new List<int>();
-            for (int i = 0; i < inputArray.Length-1; i++)
-                list.Add(inputArray[i]*inputArray[i+1]);
+            for (int i = 0; i < inputArray.Length - 1; i++)
+                list.Add(inputArray[i] * inputArray[i + 1]);
 
             return list.Max();
             //int max = -2500;
@@ -35,9 +33,18 @@ namespace CodeFights_Intro
         }
         public static int shapeArea(int n)
         {
+            //return n == 1 ? n : n * n + (n - 1) * (n - 1);
             if (n == 1) return 1;
             if (n == 2) return 5;
-            return n*n + 4;
+            return shapeArea(n - 1) + n * 4 - 4;
+            //int r = 0; // without recursion
+            //for (int i = 0; i < n - 1; i++)
+            //    r += i;
+
+            //return r * 4 + 1 + (n - 1) * 4;
+
+            //return (int)(Math.Pow(n, 2) + Math.Pow(n - 1, 2));
         }
     }
+}
 }
