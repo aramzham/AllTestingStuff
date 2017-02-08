80d21703045e5998f7da9c6a939994f1cf3a0fc1
aramzham <aramzham@hotmail.com>
2017-02-08 11:06:38 +0400

    environment class

--- Experimenting/CommandLineArguments/Program.cs
+++ Experimenting/CommandLineArguments/Program.cs
@@ -4,23 +4,51 @@ namespace CommandLineArguments
 {
     class Program
     {
-        static int Main(string[] args)
+//        static int Main(string[] args)
+//        {
+////C: \Users\HP > C:\pathToBin\bin\Debug\CommandLineArguments.exe argument1 /argument2 -argumentik3
+////enter the path to the executable file and type argument names, you'll then have the following output:
+////            Arg: argument1
+////            Arg: /argument2
+////            Arg: -argumentik3
+//            foreach (var arg in args)
+//            {
+//                Console.WriteLine($"Arg: {arg}");
+//            }
+//            //for (int i = 0; i < args.Length; i++)
+//            //{
+//            //    Console.WriteLine($"Arg: {args[i]}");
+//            //}
+//            Console.ReadLine();
+//            return -1;
+//        }
+        static void Main()
         {
-//C: \Users\HP > C:\pathToBin\bin\Debug\CommandLineArguments.exe argument1 /argument2 -argumentik3
-//enter the path to the executable file and type argument names, you'll then have the following output:
-//            Arg: argument1
-//            Arg: /argument2
-//            Arg: -argumentik3
+            //I've added some arguments in Project->Properties->Debug->Start options->Command line arguments
+            var args = Environment.GetCommandLineArgs();
             foreach (var arg in args)
             {
                 Console.WriteLine($"Arg: {arg}");
             }
-            //for (int i = 0; i < args.Length; i++)
-            //{
-            //    Console.WriteLine($"Arg: {args[i]}");
-            //}
-            Console.ReadLine();
-            return -1;
+            ShowEnvironementDetails(); //this method uses System.Environment class and tells everything about the operating system
+            Console.ReadKey();
+        }
+
+        private static void ShowEnvironementDetails()
+        {
+            foreach (var logicalDrive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine($"Drive: {logicalDrive}");
+            }
+            Console.WriteLine($"OS: {Environment.OSVersion}");
+            Console.WriteLine($"Number of processors: {Environment.ProcessorCount}");
+            Console.WriteLine($".Net version: {Environment.Version}");
+            Console.WriteLine($"Exit code: {Environment.ExitCode}");
+            Console.WriteLine($"Is OS 64-bit?: {Environment.Is64BitOperatingSystem}");
+            Console.WriteLine($"Machine name: {Environment.MachineName}");
+            Console.WriteLine($"New line symbol: {Environment.NewLine}");
+            Console.WriteLine($"System dirctory: {Environment.SystemDirectory}");
+            Console.WriteLine($"User name: {Environment.UserName}");
         }
     }
 }
