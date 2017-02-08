51e07cfda25c2d0d65d823540b42cc865325993c
aramzham <aramzham@hotmail.com>
2017-02-08 10:45:07 +0400

    processing command-line arguments

---
+++ Experimenting/CommandLineArguments/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CommandLineArguments
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+//C: \Users\HP > C:\pathToBin\bin\Debug\CommandLineArguments.exe argument1 /argument2 -argumentik3
+//enter the path to the executable file and type argument names, you'll then have the following output:
+//            Arg: argument1
+//            Arg: /argument2
+//            Arg: -argumentik3
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"Arg: {arg}");
+            }
+            //for (int i = 0; i < args.Length; i++)
+            //{
+            //    Console.WriteLine($"Arg: {args[i]}");
+            //}
+            Console.ReadLine();
+            return -1;
+        }
+    }
+}
+
+
