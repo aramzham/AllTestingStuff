98931664a86bea6a143b23f9ae012cfb74fb4748
aramzham <aramzham@hotmail.com>
2017-02-13 20:12:26 +0400

    get number from string without using Convert or Parse

---
+++ Experimenting/GetNumberWithoutConvertOrParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace GetNumberWithoutConvertOrParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "LINQ + StackOverFlow :-)";
+            Console.WriteLine(Convert("123456"));
+
+            Console.ReadKey();
+        }
+
+        public static int Convert(string number)
+        {
+            return number.Select(x => x - '0').Select((t, i) => t * (int)Math.Pow(10, number.Length - i - 1)).Sum();
+        }
+    }
+}
