e52a6e2026e89b38f6c0280f4ec9ffa04e77cd8b
aramzham <aramzham@hotmail.com>
2017-02-16 14:35:59 +0400

    try finally

---
+++ Experimenting/TryFinally/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryFinally
+{
+    class Program
+    {
+        static int x =0;
+        static string s = string.Empty;
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Method());
+            Console.WriteLine(SomeMethod());
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+
+        public static int Method()
+        {
+            try
+            {
+                return x;
+            }
+            finally
+            {
+                x = 1;
+            }
+        }
+
+        public static string SomeMethod()
+        {
+            try
+            {
+                s = "try";
+                return s;
+            }
+            finally
+            {
+                s = "finally";
+            }
+        }
+    }
+}
