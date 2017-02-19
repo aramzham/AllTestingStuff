9ef70eaf778821d61252c581ef98add91599acce
aramzham <aramzham@hotmail.com>
2017-02-19 18:29:33 +0400

    Enums

---
+++ ExperimentingOnSystemDataTypes/Enumerations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Enumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = TodoList.CallNarek;
+            Console.WriteLine(Enum.GetUnderlyingType(task.GetType()));
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(TodoList))); //without using a variable of the type
+
+            Console.ReadKey();
+        }
+        private enum TodoList : byte
+        {
+            GetFriends = 2,
+            SubscribeInGroups,
+            CallNarek = 6,
+            StickerOnDesktop
+        }
+    }
+}
