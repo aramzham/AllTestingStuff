e3d0f9a1faa558d4bd05f696e4fb2719291416f9
aramzham <aramzham@hotmail.com>
2017-02-20 13:43:52 +0400

    send by ref

--- ExperimentingOnSystemDataTypes/ValueTypeVSReferenceType/Program.cs
+++ ExperimentingOnSystemDataTypes/ValueTypeVSReferenceType/Program.cs
@@ -24,8 +24,38 @@ namespace ValueTypeVSReferenceType
             //p3.X = 88;
             //p3.Display();
             //p4.Display();
+            //Console.WriteLine(new string('-',50));
+
+            //var r1 = new Rectangle("First rec",1,1,4,4);
+            //var r2 = r1;
+
+            //r2.rectInfo.infoString = "This is a new info";  //reference type goes reference, so if we change at a place, everything wiil be changed
+            //r2.bottom = 777;  //unlike reference types, we create a copy of it and change that copy, not the original one
+
+            //r1.Display();
+            //r2.Display();
+            Console.WriteLine(new string('-', 50));
+            
+            var anthony = new Person("Martial", 21);
+            SendPersonByValue(anthony);
+            anthony.Display();
+
+            SendPersonByReference(ref anthony);
+            anthony.Display();
 
             Console.ReadKey();
+        }
+
+        static void SendPersonByValue(Person p) // a copy of the reference to the caller's object is passed
+        {
+            p.age = 100;
+            p = new Person("Nikki",20);
+        }
+
+        static void SendPersonByReference(ref Person p)
+        {
+            p.age = 888;
+            p = new Person("Nikki", 20);
         }
     }
 
@@ -63,5 +93,55 @@ namespace ValueTypeVSReferenceType
         }
     }
 
+    class ShapeInfo
+    {
+        public string infoString;
 
+        public ShapeInfo(string infoString)
+        {
+            this.infoString = infoString;
+        }
+    }
+
+    struct Rectangle
+    {
+        public ShapeInfo rectInfo;
+        public int top, left, bottom, right;
+
+        public Rectangle(string rectInfo, int top, int left, int bottom, int right)
+        {
+            this.rectInfo = new ShapeInfo(rectInfo);
+            this.top = top;
+            this.left = left;
+            this.bottom = bottom;
+            this.right = right;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"String = {rectInfo.infoString}, top = {top}, bottom = {bottom}, left = {left}, right = {right}");
+        }
+    }
+
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person()
+        {
+            
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+    }
 }
