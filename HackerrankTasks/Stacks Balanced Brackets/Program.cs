1b3aa173ec4b517c214664febd6aefdaec3827d4
aramzham <aramzham@hotmail.com>
2017-02-24 00:07:53 +0400

    bracket started

---
+++ HackerrankTasks/Stacks Balanced Brackets/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stacks_Balanced_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                string expression = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
