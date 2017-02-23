e965dd0bd2ecf6e17dee6082039e3c7cc31111ca
aramzham <aramzham@hotmail.com>
2017-02-23 11:44:04 +0400

    ctor with optional parameters

--- Encapsulation/Constructor/Program.cs
+++ Encapsulation/Constructor/Program.cs
@@ -13,8 +13,12 @@ namespace Constructor
             Person p1 = new Person();
             Person p2 = new Person("Aram");
             Person p3 = new Person(27);
-            Person p4 = new Person("Narek",26);
+            p3.SetPersonName("Gvidon");
+            Person p4 = new Person("Narek", 26);
+            Person p5 = new Person(); //default ctor is called, not the one with optional parameters
+            Person p6 = new Person(anotherField: true, age: 55);
 
+            Console.ReadKey();
         }
     }
 
@@ -22,27 +26,34 @@ namespace Constructor
     {
         private string name;
         private int age;
+        private bool anotherField;
 
         public Person(string name, int age) //master contstructor
         {
+            Console.WriteLine("Master Ctor");
             this.name = name;
             if (age < 0) age = 0;
             this.age = age;
         }
 
-        public Person(int age) : this("", age)
+        public Person(int age) : this("", age) //when chaining ctors with "this" we put all the work on one ctor - master ctor which will be called firstly, after master will be called the ctor with one parameter
         {
-
+            Console.WriteLine("Ctor with int");
         }
 
         public Person(string name) : this(name, 0)
         {
-
+            Console.WriteLine("Ctor with string");
         }
 
         public Person()
         {
-            
+            Console.WriteLine("Default ctor");
+        }
+
+        public Person(string name = "Babo", int age = 40, bool anotherField = false)
+        {
+            Console.WriteLine("Ctor with optional parameters");
         }
 
         public void SetPersonName(string value)
