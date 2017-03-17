b6e288515b6bd9c9a09717f2e28d01b3167e3971
aramzham <aramzham@hotmail.com>
2017-03-17 21:48:01 +0400

    obj init 2

--- Encapsulation/ObjectInitialization/Program.cs
+++ Encapsulation/ObjectInitialization/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ObjectInitialization
 {
@@ -23,6 +19,19 @@ namespace ObjectInitialization
             p3.Y = 60;
             p3.Display();
 
+            var p4 = new Point(70, 80) { X = 90, Y = 100 }; //not a great use
+            p4.Display();
+
+            var p5 = new Point(PointColor.LightBlue) {X = 110,Y = 120}; //there is already a sense
+            p5.Display();
+
+            var rectangle = new Rectangle()
+            {
+                BottomRight = new Point() {X = 10, Y = 20,PointColor = PointColor.Gold},
+                TopLeft = new Point() {X = 100, Y = 200, PointColor = PointColor.BloodRed}
+            };
+            rectangle.DisplayStats();
+
             Console.ReadKey();
         }
     }
@@ -36,17 +45,42 @@ namespace ObjectInitialization
             Y = y;
         }
 
-        public Point()
+        public Point() : this(PointColor.BloodRed)
         {
             Console.WriteLine("Default ctor is called");
         }
+
+        public Point(PointColor pointColor)
+        {
+            Console.WriteLine("One parameter ctor is called");
+            PointColor = pointColor;
+        }
         public int X { get; set; }
         public int Y { get; set; }
+        public PointColor PointColor { get; set; }
 
         public void Display()
         {
             Console.WriteLine($"X = {X}, Y = {Y}");
         }
+    }
+    class Rectangle
+    {
+        public Point TopLeft { get; set; } = new Point();
+        public Point BottomRight { get; set; } = new Point();
 
+        public void DisplayStats()
+        {
+            Console.WriteLine("[TopLeft: {0}, {1}, {2} BottomRight: {3}, {4}, {5}]",
+            TopLeft.X, TopLeft.Y, TopLeft.PointColor,
+            BottomRight.X, BottomRight.Y, BottomRight.PointColor);
+        }
+    }
+
+    enum PointColor
+    {
+        LightBlue,
+        BloodRed,
+        Gold
     }
 }
