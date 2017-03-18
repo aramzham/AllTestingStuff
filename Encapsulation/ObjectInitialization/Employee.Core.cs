ada699361e6022725c5c1ae534b299a55ce37902
aramzham <aramzham@hotmail.com>
2017-03-18 15:21:01 +0400

    partial class employee

---
+++ Encapsulation/ObjectInitialization/Employee.Core.cs
@@ -0,0 +1,18 @@
+﻿namespace ObjectInitialization
+{
+    partial class Employee //name the file -name-.-something-.cs to make it partial with -name-.cs file
+    {
+        public Employee() : this(0,"")
+        {
+            
+        }
+
+        public Employee(int salary, string name)
+        {
+            Salary = salary;
+            Name = name;
+        }
+        public int Salary { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Encapsulation/ObjectInitialization/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ObjectInitialization
+{
+    partial class Employee
+    {
+        public void GetBonus(int sum)
+        {
+            Salary += sum;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{Name} gets {Salary}$ salary");
+        }
+    }
+}
