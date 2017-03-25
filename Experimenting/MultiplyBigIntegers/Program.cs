c1f8f6ad18ce953d416f9c52663d4ef51db1a60f
aramzham <aramzham@hotmail.com>
2017-03-25 14:49:57 +0400

    html1

--- Experimenting/MultiplyBigIntegers/Program.cs
+++ Experimenting/MultiplyBigIntegers/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace MultiplyBigIntegers
 {
