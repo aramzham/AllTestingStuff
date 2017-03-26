11044fe7ada300ee6877c0bcf95cf2776a108afd
aramzham <aramzham@hotmail.com>
2017-03-26 12:58:16 +0400

    tests opened

---
+++ Experimenting/TestsOfAllKinds/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsOfAllKinds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
