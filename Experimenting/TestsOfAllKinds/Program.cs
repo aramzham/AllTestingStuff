94f580e78fc71688d917e318130da89c9e9e2b46
aramzham <aramzham@hotmail.com>
2017-05-21 12:37:49 +0400

    unnecessary Larry removed
    Signed-off-by: aramzham <aramzham@hotmail.com>

--- Experimenting/TestsOfAllKinds/Program.cs
+++ Experimenting/TestsOfAllKinds/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestsOfAllKinds
 {
+// Math.Floor() function:   The two numbers 123.456 and 123.987 are rounded down to the nearest integer.This means that regardless of how close they are close to 124, they are rounded to 123.
+//Note:
+//Floor can be useful when rounding numbers that are part of a larger representation of another number.
+
+//Discussion.The Math.Floor method when given a positive number will erase the digits after the decimal place. But when it receives a negative number, it will erase the digits and increase the number's negativity by 1.
+//So:
+//Using Math.Floor on a negative number will still decrease the total number.This means it will always become smaller.
     class Program
     {
         static void Main(string[] args)
