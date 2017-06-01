790ea7c2999baf46cb1bbc38965f16d26dae0435
aramzham <aramzham@hotmail.com>
2017-06-01 18:56:07 +0400

    selection sort need to be done

--- AlgorithmsLesson/LookForANumber/Program.cs
+++ AlgorithmsLesson/LookForANumber/Program.cs
@@ -10,38 +10,71 @@ namespace LookForANumber
     {
         static void Main(string[] args)
         {
-            var rnd = new Random();
-            var size = rnd.Next(6, 24);
-            var a = new int[size];
-            Console.WriteLine($"Size = {size}");
-            for (int i = 0; i < a.Length; i++)
-            {
-                a[i] = rnd.Next(-99, 100);
-            }
-            Array.Sort(a);
-            foreach (var x in a)
-            {
-                Console.Write($"{x}\t");
-            }
+            //var rnd = new Random();
+            //var size = rnd.Next(6, 24);
+            //var a = new int[size];
+            //Console.WriteLine($"Size = {size}");
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    a[i] = rnd.Next(-99, 100);
+            //}
+            //Array.Sort(a);
+            //foreach (var x in a)
+            //{
+            //    Console.Write($"{x}\t");
+            //}
 
-            do
-            {
-                Console.WriteLine("Enter a number");
-                var n = int.Parse(Console.ReadLine());
-                Console.WriteLine(FindBinary(a, 0, a.Length - 1, n));
-            } while (true);
+            //do
+            //{
+            //    Console.WriteLine("Enter a number");
+            //    var n = int.Parse(Console.ReadLine());
+            //    //Console.WriteLine(FindBinary(a, 0, a.Length - 1, n));
+            //    Console.WriteLine(FindBinary(a,20));
+            //} while (true);
+
+            var a = new int[] {7, 91, 125, 430, 530};
+            var n = 12;
+            Console.WriteLine(FindBinary(a,n));
 
             Console.ReadKey();
         }
 
         private static int FindBinary(int[] array, int begin, int end, int target)
         {
-            if (begin > end) return -1;
-            var middle = begin + (end - begin) / 2;
-            if (array[middle] == target) return middle;
-            if (array[middle] > target) return FindBinary(array, begin, middle - 1, target);
-            else return FindBinary(array, middle + 1, end, target);
+            //if (begin > end) return -1;
+            //var middle = begin + (end - begin) / 2;
+            //if (array[middle] == target) return middle;
+            //if (array[middle] > target) return FindBinary(array, begin, middle - 1, target);
+            //else return FindBinary(array, middle + 1, end, target);
+
+            while (true)
+            {
+                if (begin > end) return -1;
+                var middle = begin + (end - begin) / 2;
+
+                if (array[middle] == target) return middle;
+                if (array[middle] > target) end = middle - 1;
+                else begin = middle + 1;
+            }
         }
+
+        private static int FindBinary(int[] array, int n)
+        {
+            var first = 0;
+            var last = array.Length - 1;
+            var middle = last/2;
+
+            while (first<=last)
+            {
+                if (n == array[middle]) return middle;
+                if (n > array[middle]) first = middle + 1;
+                else last = middle - 1;
+
+                middle = first + (last - first)/2;
+            }
+            return -1;
+        }
+
         // int[] array
         // begin-------------------------middle--------------------------end
     }
---
+++ AlgorithmsLesson/SelectionSort/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        //TODO: hashvel sra bardutyun@
+        //TODO: nayel bubble algorithm@, grel code-@
+        static void Main(string[] args)
+        {
+            foreach (var item in new int[] { 3, 1, 20, 5 })
+            {
+                Console.Write($"{item}\t");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int Max(int[] a, int j)
+        //private static int Max(int[] a)
+        {
+            var mx = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > a[mx]) mx = i;
+            }
+            return mx;
+            //return Array.IndexOf(a, a.Max());
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            var t = a;
+            a = b;
+            b = t;
+        }
+
+        private static int[] SelSort(int[] a)
+        {
+            var maxElement = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                //maxElement = Max(a, i);
+                maxElement = Max(a, i);
+                Swap(ref a[maxElement], ref a[a.Length - i - 1]);
+            }
+            return a;
+        }
+    }
+}
