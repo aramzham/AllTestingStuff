2b566b03b4b399f43cedb5c0ab4bbf09cd6f1e2b
aramzham <aramzham@hotmail.com>
2017-07-30 00:01:16 +0400

    nt service init

---
+++ Experimenting/FirstService/MyServiceInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace FirstService
+{
+    [RunInstaller(true)]
+    public class MyServiceInstaller : Installer
+    {
+        public MyServiceInstaller()
+        {
+            this.Installers.Add(new ServiceProcessInstaller { Account = ServiceAccount.LocalSystem });
+            this.Installers.Add(new ServiceInstaller { ServiceName = "===== TEST SERVICE =====", Description = "My First NT Service!", StartType = ServiceStartMode.Automatic });
+        }
+    }
+}
