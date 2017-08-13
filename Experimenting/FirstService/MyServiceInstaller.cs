2a07461b265b433dc83c9753a5698587dba26798
aramzham <aramzham@hotmail.com>
2017-08-13 11:13:18 +0400

    1 to bi association

--- Experimenting/FirstService/MyServiceInstaller.cs
+++ Experimenting/FirstService/MyServiceInstaller.cs
@@ -13,4 +13,41 @@ namespace FirstService
             this.Installers.Add(new ServiceInstaller { ServiceName = "===== TEST SERVICE =====", Description = "My First NT Service!", StartType = ServiceStartMode.Automatic });
         }
     }
+    //// Выбор файла-службы
+    //private void Button3_Click(object sender, EventArgs e)
+    //{
+    //    if (openFileDialog1.ShowDialog() == DialogResult.OK)
+    //    {
+    //        textBox1.Text = openFileDialog1.SafeFileName;
+    //    }
+    //}
+    //// Устанавка службы.
+    //private void Button1_Click(object sender, EventArgs e)
+    //{
+    //        try
+    //        {
+    //            ManagedInstallerClass.InstallHelper(new[]
+    //                                                    {
+    //                                                            openFileDialog1.FileName
+    //                                                        });
+    //            MessageBox.Show("Сервис " + openFileDialog1.SafeFileName + " установлен!");
+    //        }
+    //        catch (Exception exc)
+    //        {
+    //            MessageBox.Show(exc.ToString());
+    //        }
+    //}
+    //// Деинсталяция службы.
+    //private void Button2_Click(object sender, EventArgs e)
+    //{
+    //        try
+    //        {
+    //            ManagedInstallerClass.InstallHelper(new[] { @"/u", openFileDialog1.FileName });
+    //            MessageBox.Show("Сервис " + openFileDialog1.SafeFileName + " деинсталирован!");
+    //        }
+    //        catch (Exception exc)
+    //        {
+    //            MessageBox.Show(exc.ToString());
+    //        }
+    //}
 }
