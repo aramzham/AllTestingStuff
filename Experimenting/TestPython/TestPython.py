b2a05e88c799670f80799806f01cda4480bb0d1b
Aram Zhamkochyan <aramzham@hotmail.com>
2017-10-08 13:29:28 +0400

    multiple inheritance example

--- Experimenting/TestPython/TestPython.py
+++ Experimenting/TestPython/TestPython.py
@@ -15,3 +15,25 @@ zerosCount = CountTrailingZeros(zeros)
 
 
 print(zerosCount)
+
+class Horse():
+    def Run(self):
+        print('I am running')
+
+class Bird():
+    def Run(self):
+        print("Yeah, I can run too")
+
+    def Fly(self):
+        print('I am flying')
+
+class Pegasus(Bird,Horse):
+    pass
+
+def main():
+    pegasus = Pegasus()
+    pegasus.Fly()
+    pegasus.Run()
+
+if __name__ == "__main__":
+    main()  
