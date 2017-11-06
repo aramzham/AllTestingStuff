24c3350664b9428711286792c005fa2f2fa8f079
Aram Zhamkochyan <aramzham@hotmail.com>
2017-11-06 21:41:16 +0400

    matrix power

--- TestsInPython/Vectors/Vectors.py
+++ TestsInPython/Vectors/Vectors.py
@@ -2,25 +2,11 @@ def double_the_vector(vector):
     multiplyer = 2
     return list(map(lambda x: multiplyer * x,vector))
 
-vector = [1,3,10]
-print(double_the_vector(vector))
-
 def axpy(a,x,y):
     return [a * x[i] + y[i] for i in range(0,len(x))]
 
-a = -1
-x = [1,34,4]
-y = [2,4,6]
-
-print(axpy(a,x,y))
-
 def dot(x,y):
     return sum([x[i] * y[i] for i in range(0,len(x))])
-
-x = [90,30,10,15,5]
-y = [1.4,1.2,0.6,0.2,2.0]
-
-print(dot(x,y))
 
 def length(vector):
     return dot(vector,vector) ** 0.5
@@ -36,11 +22,6 @@ def algorithm_dot(x,y):
 
     return a
 
-x = [2,-9,8]
-y = [1,0,-1]
-
-print(algorithm_dot(x,y))
-
 def algorithm_dot(a,x,y):
     result_vector = []
     index = 0
@@ -49,8 +30,6 @@ def algorithm_dot(a,x,y):
         index+=1
 
     return result_vector
-
-print(algorithm_dot(-1,[2,-1,4,2,1],[1,-2,2,3,-1]))
     
 from random import *
 def create_random_matrix(m,n):
@@ -63,8 +42,6 @@ def create_random_matrix(m,n):
         del row
 
     return matrix
-
-print(create_random_matrix(5,3))
 
 def set_matrix_to_zero(matrix):
     if(len(matrix) <= 0): raise ValueError
@@ -83,9 +60,6 @@ def set_matrix_to_identity(matrix):
             else: matrix[i][j] = 0
 
     return matrix
-
-matrix = create_random_matrix(3,3)
-print(set_matrix_to_identity(matrix))
 
 def create_diagonal_matrix(*params):
     if(params == None or len(params) == 0): raise ValueError
@@ -152,6 +126,24 @@ def traigular_matrix_vector_multiplication(matrix, vector):
 
     return resultMatrix
 
-ltm = create_lower_triangular_matrix(3)
-vector = [[-1],[0],[2]]
-print(traigular_matrix_vector_multiplication(vector, ltm))
+def matrix_multiplication(matrix1, matrix2):
+    if(len(matrix1)<1 or len(matrix1[0])!=len(matrix2)): raise ValueError
+
+    matrix = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix2[0])):
+            row.append(dot(matrix1[i],list(map(lambda x:x[j],matrix2))))
+        matrix.append(row)
+        del row
+
+    return matrix
+
+def matrix_power(matrix,n):
+    if(len(matrix)<1 or len(matrix)!=len(matrix[0])): raise ValueError("Matrix must be square!")
+
+    original_matrix = matrix
+    for i in range(n):
+        matrix = matrix_multiplication(original_matrix, matrix)
+
+    return matrix
