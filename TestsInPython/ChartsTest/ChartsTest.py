c0f51397c1eaf90f5bdbc981b8d2ca2ec3db9ce5
Aram Zhamkochyan <aramzham@hotmail.com>
2017-11-12 20:17:56 +0400

    homework part 1

--- TestsInPython/ChartsTest/ChartsTest.py
+++ TestsInPython/ChartsTest/ChartsTest.py
@@ -1,16 +1,14 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+ecom = pd.read_csv(r'C:\Users\Aram\Downloads\Ecommerce Purchases')
+print(ecom.columns.values)
+print(['Address', 'Lot', 'AM or PM', 'Browser Info', 'Company',
+       'Credit Card', 'CC Exp Date', 'CC Security Code', 'CC Provider',
+       'Email', 'Job', 'IP Address', 'Language', 'Purchase Price'])
 
-Height=[65.78, 71.52, 69.4, 68.22, 67.79, 68.7, 69.8, 70.01, 67.9, 66.78,
- 66.49, 67.62, 68.3, 67.12, 68.28, 71.09, 66.46, 68.65, 71.23, 67.13, 67.83, 
-68.88, 63.48, 68.42, 67.63, 67.21, 70.84, 67.49, 66.53, 65.44, 69.52, 65.81, 
-67.82, 70.6, 71.8, 69.21, 66.8, 67.66, 67.81, 64.05, 68.57, 65.18, 69.66, 67.97, 
-65.98, 68.67, 66.88, 67.7, 69.82, 69.09]
-Weight=[112.99, 136.49, 153.03, 142.34, 144.3, 123.3, 141.49, 136.46, 
-112.37, 120.67, 127.45, 114.14, 125.61, 122.46, 116.09, 140.0, 129.5, 142.97, 
-137.9, 124.04, 141.28, 143.54, 97.9, 129.5, 141.85, 129.72, 142.42, 131.55, 
-108.33, 113.89, 103.3, 120.75, 125.79, 136.22, 140.1, 128.75, 141.8, 121.23, 
-131.35, 106.71, 124.36, 124.86, 139.67, 137.37, 106.45, 128.76, 145.68, 116.82, 
-143.62, 134.93]
+print(len([_ for _ in filter(lambda x: x[0].split('/')[1]=='25',ecom[['CC Exp Date']].values)]))
 
-plt.scatterplot(Weight)
-plt.show()
+l = list(map(lambda x: x[0].split('@')[1],ecom[['Email']].values))
+
+from collections import Counter
+print(list(Counter(l))[:5])
