348ccf124bf2a79c93d33758f95f2e9edb7dddd8
Aram Zhamkochyan <aramzham@hotmail.com>
2017-11-12 23:36:08 +0400

    homework part 2

--- TestsInPython/ChartsTest/ChartsTest.py
+++ TestsInPython/ChartsTest/ChartsTest.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
-ecom = pd.read_csv(r'C:\Users\Aram\Downloads\Ecommerce Purchases')
-print(ecom.columns.values)
-print(['Address', 'Lot', 'AM or PM', 'Browser Info', 'Company',
-       'Credit Card', 'CC Exp Date', 'CC Security Code', 'CC Provider',
-       'Email', 'Job', 'IP Address', 'Language', 'Purchase Price'])
+sal = pd.read_csv(r'C:\Users\Aram\Downloads\Salaries.csv')
+print(sal.columns.values)
+print(['Id' 'EmployeeName' 'JobTitle' 'BasePay' 'OvertimePay' 'OtherPay'
+ 'Benefits' 'TotalPay' 'TotalPayBenefits' 'Year' 'Notes' 'Agency' 'Status'])
 
-print(len([_ for _ in filter(lambda x: x[0].split('/')[1]=='25',ecom[['CC Exp Date']].values)]))
+#print(len([_ for _ in filter(lambda x: x[0].split('/')[1]=='25',ecom[['CC Exp Date']].values)]))
 
-l = list(map(lambda x: x[0].split('@')[1],ecom[['Email']].values))
+#l = list(map(lambda x: x[0].split('@')[1],ecom[['Email']].values))
 
-from collections import Counter
-print(list(Counter(l))[:5])
+#from collections import Counter
+#print(list(Counter(l))[:5])
+sal['BasePay'].fillna(sal['BasePay'].mean())
+print(np.average(sal['BasePay']))
+print(np.max(sal['OvertimePay']))
+print(sal[sal['EmployeeName']=='JOSEPH DRISCOLL'])
+len([_ for _ in filter(lambda x: x.index().lower().contains('cheif'),sal['JobTitle'].value_counts())])
