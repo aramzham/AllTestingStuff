169b382d836530d4d40c1baad64f0b94c6a7f297
Aram Zhamkochyan <aramzham@hotmail.com>
2017-11-14 11:41:05 +0400

    pyplot installed

--- TestsInPython/ChartsTest/ChartsTest.py
+++ TestsInPython/ChartsTest/ChartsTest.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pandas as pd
-ecom = pd.read_csv(r'C:\Users\Aram\Downloads\Ecommerce Purchases')
-print(ecom.columns.values)
-print(['Address', 'Lot', 'AM or PM', 'Browser Info', 'Company',
-       'Credit Card', 'CC Exp Date', 'CC Security Code', 'CC Provider',
-       'Email', 'Job', 'IP Address', 'Language', 'Purchase Price'])
+#ecom = pd.read_csv(r'C:\Users\Aram\Downloads\Ecommerce Purchases')
+#print(ecom.columns.values)
+#print(['Address', 'Lot', 'AM or PM', 'Browser Info', 'Company',
+#       'Credit Card', 'CC Exp Date', 'CC Security Code', 'CC Provider',
+#       'Email', 'Job', 'IP Address', 'Language', 'Purchase Price'])
 
-print(len([_ for _ in filter(lambda x: x[0].split('/')[1]=='25',ecom[['CC Exp Date']].values)]))
+#print(len([_ for _ in filter(lambda x: x[0].split('/')[1]=='25',ecom[['CC Exp Date']].values)]))
 
-l = list(map(lambda x: x[0].split('@')[1],ecom[['Email']].values))
+#l = list(map(lambda x: x[0].split('@')[1],ecom[['Email']].values))
 
-from collections import Counter
-print(list(Counter(l))[:5])
+#from collections import Counter
+#print(list(Counter(l))[:5])
+
+import matplotlib as plt
+fig = plt.figure()
+
