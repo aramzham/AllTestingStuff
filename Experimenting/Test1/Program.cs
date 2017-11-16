69259531e0d7765311d720e5c334102f0e646664
Aram Zhamkochyan <aramzham@hotmail.com>
2017-11-16 19:00:23 +0400

    almost increasing try 1

---
+++ Experimenting/Test1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 1, 3, 2, 1 };
+            Console.WriteLine(almostIncreasingSequence(array));
+
+            Console.ReadKey();
+        }
+        static bool almostIncreasingSequence(int[] sequence)
+        {
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                var list = sequence.ToList();
+                list.RemoveAt(i);
+                if (list.OrderBy(x => x).SequenceEqual(list)) return true;
+            }
+            return false;
+        }
+    }
+}
