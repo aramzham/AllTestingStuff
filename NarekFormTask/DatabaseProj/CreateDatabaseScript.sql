0ba8e8051f18b396a06091e37be74d87d7ed0e92
Aram Zhamkochyan <aramzham@hotmail.com>
2018-03-16 23:00:38 +0400

    narek task started

---
+++ NarekFormTask/DatabaseProj/CreateDatabaseScript.sql
@@ -0,0 +1,10 @@
+﻿use master
+go
+
+CREATE DATABASE [TestTaskDb] ON  PRIMARY 
+( NAME = N'TestTaskDb', FILENAME = 'D:\Aram\Programming\Databases\TestTaskDb.mdf' , 
+  SIZE = 100MB , MAXSIZE = 1GB, FILEGROWTH = 20MB )
+LOG ON 
+( NAME = N'TestTaskDb_log', FILENAME = 'D:\Aram\Programming\Databases\TestTaskDb_log.ldf' , 
+  SIZE = 100MB , MAXSIZE = 1GB, FILEGROWTH = 10%)
+GO
