354a4f36207f7c744fee1c1310b62e71fc73f5b7
Aram Zhamkochyan <aramzham@hotmail.com>
2018-03-18 12:53:52 +0400

    web api installed

---
+++ NarekFormTask/NFT.WebApi/Infrastructure/DB_Manager.cs
@@ -0,0 +1,45 @@
+﻿using NFT.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NFT.WebApi.Infrastructure
+{
+    public class DB_Manager
+    {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["EmployeeDb"].ConnectionString;
+
+        public async Task<List<Employee>> GetAllEmployees()
+        {
+            const string query = "select * from employee";
+            var employees = new List<Employee>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                connection.Open();
+
+                var reader = await command.ExecuteReaderAsync();
+                while (await reader.ReadAsync())
+                {
+                    employees.Add(new Employee()
+                    {
+                        Id = (int)reader[0],
+                        Name = (string)reader[1],
+                        Surname = (string)reader[2],
+                        IsGettingBonus = (bool)reader["IsGettingBonus"],
+                        Salary = (decimal)reader["Salary"],
+                        UniversityId = (int)reader["UniversityId"],
+                        Info = reader["Info"] as string
+                    });
+                }
+                reader.Close();
+            }
+            return employees;
+        }
+    }
+}
