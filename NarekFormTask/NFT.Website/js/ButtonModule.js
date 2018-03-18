af4af969365e7c6fd635b13d86da215f749ca62e
Aram Zhamkochyan <aramzham@hotmail.com>
2018-03-18 23:18:17 +0400

    js proceed

--- NarekFormTask/NFT.Website/js/ButtonModule.js
+++ NarekFormTask/NFT.Website/js/ButtonModule.js
@@ -1,4 +1,4 @@
-﻿function GetAllEmployees() {
+﻿function getEmployees() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://localhost:59406/api/employee", true);
     xhr.onreadystatechange = function () {
@@ -9,15 +9,28 @@
     xhr.send();
 }
 
-function GetEmployeeById() {
+function getEmployeeById() {
     var id = document.getElementById("idBox").value;
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost:59406/api/employee?id={id}".replace("{id}",id), true);
+    xhr.open("GET", "http://localhost:59406/api/employee?id={id}".replace("{id}", id), true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            document.getElementById("mainTable").innerHTML = xhr.responseText;
+            fillTable(xhr.responseText);
         }
     }
     xhr.send();
+}
+
+function fillTable(list) {
+    var tbody = document.getElementById("mainTable").children[1];
+    //var tbody = table.getElementsByTagName("tbody")[0];
+    tbody.innerHTML = "";
+    if (list instanceof Array) {
+
+    }
+    else {
+        var json = JSON.parse(list);
+        tbody.innerHTML = "<tr>" + "<td>" + json["name"] + "</td>" + "<td>" + json.surname + "</td>" + "</tr>";
+    }
 }
--- NarekFormTask/NFT.Website/js/init.js
+++ NarekFormTask/NFT.Website/js/init.js
@@ -1,6 +1,6 @@
 ﻿(function () {
     window.onload = function () {
-        document.getElementsByName("getAll")[0].onclick = GetAllEmployees;
-        document.getElementsByName("getById")[0].onclick = GetEmployeeById;
+        document.getElementById("getAll").onclick = getEmployees;
+        document.getElementById("getById").onclick = getEmployeeById;
     }
 })();
