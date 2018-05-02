32c465eb915a4feb57da0500bc0201f09e112eaf
Aram Zhamkochyan <aramzham@hotmail.com>
2018-05-02 23:12:56 +0400

    clock try 1

---
+++ AspNetCore/TestWebApp/Clock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp
+{
+    public class Clock : IClock
+    {
+        public DateTime GetTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ AspNetCore/TestWebApp/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp
+{
+    public class HomeController : Controller
+    {
+        private IClock _clock;
+
+        public HomeController(IClock clock)
+        {
+            _clock = clock;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = $"It is {_clock.GetTime().ToString("T")}";
+            return View();
+        }
+    }
+}
