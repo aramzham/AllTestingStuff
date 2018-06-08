e3f2db385e23067a8e087151e061f2af0e002c19
Aram Zhamkochyan <aramzham@hotmail.com>
2018-06-08 11:45:16 +0400

    up to editors

---
+++ MvcTesting/MVCSuccinctly/Models/ItineraryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSuccinctly.Models
+{
+    public class ItineraryItem
+    {
+        public DateTime? When { get; set; }
+        public string Description { get; set; }
+        public int? Duration { get; set; }
+    }
+}
