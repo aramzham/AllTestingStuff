75cc9e57f3a4bdfc0b6b9ee7d6adabd2000ec227
Aram Zhamkochyan <aramzham@hotmail.com>
2018-06-11 14:55:07 +0400

    data anotation editors

--- MvcTesting/MVCSuccinctly/Models/ItineraryItem.cs
+++ MvcTesting/MVCSuccinctly/Models/ItineraryItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace MVCSuccinctly.Models
     public class ItineraryItem
     {
         public DateTime? When { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public int? Duration { get; set; }
+        public bool IsActive { get; set; }
+        public bool? Confirmed { get; set; }
     }
 }
