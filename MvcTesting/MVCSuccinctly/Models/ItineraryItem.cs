1a2f047dc50f2618262269476b4f32a1eea4e5fc
Aram Zhamkochyan <aramzham@hotmail.com>
2018-06-12 16:53:02 +0400

    up to regex

--- MvcTesting/MVCSuccinctly/Models/ItineraryItem.cs
+++ MvcTesting/MVCSuccinctly/Models/ItineraryItem.cs
@@ -8,11 +8,20 @@ namespace MVCSuccinctly.Models
 {
     public class ItineraryItem
     {
+        [Required(ErrorMessage ="You must specify when the event will occur")]
         public DateTime? When { get; set; }
-        [DataType(DataType.MultilineText)]
+
+        [Required(ErrorMessage = "You must enter a description"), MaxLength(140, ErrorMessage ="The description must be less than 140 characters."), DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "You must specify how long the event will last"), Range(1, 120, ErrorMessage ="Events should last between one minute and 2 hours"), RegularExpression(@"\d{1,3}", ErrorMessage = "Only numbers are allowed in the duration")]
         public int? Duration { get; set; }
+
         public bool IsActive { get; set; }
+
         public bool? Confirmed { get; set; }
+
+        //[UIHint("Phone")]
+        public string ContactNumber { get; set; }
     }
 }
