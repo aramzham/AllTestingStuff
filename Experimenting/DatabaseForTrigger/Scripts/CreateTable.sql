5b46130e966053a9abd11f945066f766f00a7af6
Aram Zhamkochyan <aramzham@hotmail.com>
2018-09-24 23:38:32 +0400

    trigger tests

---
+++ Experimenting/DatabaseForTrigger/Scripts/CreateTable.sql
@@ -0,0 +1,29 @@
+﻿create table Test(
+Id int identity not null,
+[Name] varchar(450) not null,
+Age int null
+)
+
+insert into Test
+values
+('Aram', 29)
+
+select * from Test
+
+create table OtherTable(
+Id int identity not null,
+[Name] varchar(450) not null,
+Age int null,
+[Action] varchar(450) not null,
+CreationDate datetime default getdate()
+)
+
+insert into Test
+values
+('Narek', 29)
+
+update Test
+set name =  'Aram'
+where id = 7
+
+select * from OtherTable
---
+++ Experimenting/DatabaseForTrigger/Scripts/Trigger.sql
@@ -0,0 +1,26 @@
+﻿--drop trigger insert_trigger_Test
+
+create trigger insert_trigger_Test
+on Test
+after insert, update
+as
+declare @ins int, @del int
+select @ins = count(*) from inserted
+select @del = count(*) from deleted
+
+if @ins > 0 and @del = 0
+begin
+	insert into OtherTable
+	([Name], Age, [Action])
+	select [Name], Age, 'inserted' from inserted
+end
+else if @ins>0 and @del > 0
+begin
+	insert into OtherTable
+	([Name], Age, [Action])
+	select [Name], Age, 'inserted' from inserted
+
+	insert into dbo.OtherTable
+	([Name], Age, [Action])
+	select [Name], Age, 'deleted' from deleted
+end
