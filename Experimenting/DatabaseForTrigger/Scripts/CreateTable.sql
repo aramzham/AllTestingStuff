0a7a048dd1397c7f2488c2d8eb0368908fcdb23e
Aram Zhamkochyan <aramzham@hotmail.com>
2018-09-26 22:36:17 +0400

    not a good idea to put export function in trigger, so you'll need a job to write to file in every given time

--- Experimenting/DatabaseForTrigger/Scripts/CreateTable.sql
+++ Experimenting/DatabaseForTrigger/Scripts/CreateTable.sql
@@ -4,12 +4,6 @@ Id int identity not null,
 Age int null
 )
 
-insert into Test
-values
-('Aram', 29)
-
-select * from Test
-
 create table OtherTable(
 Id int identity not null,
 [Name] varchar(450) not null,
@@ -17,13 +11,3 @@ Age int null,
 [Action] varchar(450) not null,
 CreationDate datetime default getdate()
 )
-
-insert into Test
-values
-('Narek', 29)
-
-update Test
-set name =  'Aram'
-where id = 7
-
-select * from OtherTable
