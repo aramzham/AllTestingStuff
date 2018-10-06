b72701cae2aea1081b588c4a06ca3d946b749066
Aram Zhamkochyan <aramzham@hotmail.com>
2018-10-06 11:39:39 +0400

    linkedin contact adder

---
+++ Experimenting/BringMeSomeContacts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+
+namespace BringMeSomeContacts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.linkedin.com/mynetwork/");
+            while (true)
+            {
+                while (count != 100)
+                {
+                    driver.ExecuteScript("window.scrollTo(0, 12000)");
+                    Thread.Sleep(1000);
+                    count++;
+                }
+                var buttons = driver.FindElementsByXPath(".//button[@data-control-name='invite']");
+                foreach (var button in buttons)
+                {
+                    button.Click();
+                    Thread.Sleep(1000);
+                } 
+            }
+        }
+    }
+}
