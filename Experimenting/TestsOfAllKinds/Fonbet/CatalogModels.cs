5c11ba0edbfc1ef0596394662b3f1d8bfe6d8d39
Aram Zhamkochyan <aramzham@hotmail.com>
2018-11-03 23:22:20 +0400

    fonbet models added

---
+++ Experimenting/TestsOfAllKinds/Fonbet/CatalogModels.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsOfAllKinds.Fonbet
+{
+
+    public class RootCatalogObject
+    {
+        public int catalogVersion { get; set; }
+        public Catalog[] catalog { get; set; }
+    }
+
+    public class Catalog
+    {
+        public string name_rus { get; set; }
+        public string name_eng { get; set; }
+        public Grid[] grids { get; set; }
+    }
+
+    public class Grid
+    {
+        public int num { get; set; }
+        public string name_rus { get; set; }
+        public string name_eng { get; set; }
+        public int f { get; set; }
+        public Grid1[][] grid { get; set; }
+        public bool hasBuyFactor { get; set; }
+        public bool sortByParam { get; set; }
+    }
+
+    public class Grid1
+    {
+        public string rus { get; set; }
+        public string eng { get; set; }
+        public string kind { get; set; }
+        public int factorId { get; set; }
+        public int a { get; set; }
+        public string b_rus { get; set; }
+        public string b_eng { get; set; }
+        public bool flexBet { get; set; }
+        public bool flexParam { get; set; }
+        public string e_rus { get; set; }
+        public string e_eng { get; set; }
+    }
+}
---
+++ Experimenting/TestsOfAllKinds/Fonbet/FonbetJsonModels.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsOfAllKinds.Fonbet
+{
+
+    public class RootModelObject
+    {
+        public int packetVersion { get; set; }
+        public int fromVersion { get; set; }
+        public int factorsVersion { get; set; }
+        public int specialLineCatalogVersion { get; set; }
+        public int siteVersion { get; set; }
+        public Sport[] sports { get; set; }
+        public Event[] events { get; set; }
+        public Eventblock[] eventBlocks { get; set; }
+        public Eventmisc[] eventMiscs { get; set; }
+        public Customfactor[] customFactors { get; set; }
+        public Announcement[] announcements { get; set; }
+    }
+
+    public class Sport
+    {
+        public int id { get; set; }
+        public int parentId { get; set; }
+        public string kind { get; set; }
+        public int regionId { get; set; }
+        public string sortOrder { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Event
+    {
+        public int id { get; set; }
+        public int parentId { get; set; }
+        public string sortOrder { get; set; }
+        public int level { get; set; }
+        public int num { get; set; }
+        public int sportId { get; set; }
+        public int kind { get; set; }
+        public int rootKind { get; set; }
+        public int team1Id { get; set; }
+        public int team2Id { get; set; }
+        public string name { get; set; }
+        public string namePrefix { get; set; }
+        public int startTime { get; set; }
+        public string place { get; set; }
+        public int priority { get; set; }
+        public State state { get; set; }
+        public string team1 { get; set; }
+        public string team2 { get; set; }
+    }
+
+    public class State
+    {
+        public bool inHotList { get; set; }
+        public bool willBeLive { get; set; }
+    }
+
+    public class Eventblock
+    {
+        public int eventId { get; set; }
+        public string state { get; set; }
+    }
+
+    public class Eventmisc
+    {
+        public int id { get; set; }
+        public int liveDelay { get; set; }
+        public int score1 { get; set; }
+        public int score2 { get; set; }
+        public int servingTeam { get; set; }
+        public string comment { get; set; }
+        public int timerDirection { get; set; }
+        public int timerSeconds { get; set; }
+        public int[] tv { get; set; }
+        public int timerUpdateTimestamp { get; set; }
+        public long timerUpdateTimestampMsec { get; set; }
+    }
+
+    public class Customfactor
+    {
+        public int e { get; set; }
+        public int f { get; set; }
+        public float v { get; set; }
+        public int p { get; set; }
+        public string pt { get; set; }
+        public bool isLive { get; set; }
+        public int lo { get; set; }
+        public int hi { get; set; }
+    }
+
+    public class Announcement
+    {
+        public int id { get; set; }
+        public int num { get; set; }
+        public int segmentId { get; set; }
+        public string segmentName { get; set; }
+        public int sportId { get; set; }
+        public string segmentSortOrder { get; set; }
+        public int team1Id { get; set; }
+        public int team2Id { get; set; }
+        public string team1 { get; set; }
+        public string team2 { get; set; }
+        public string name { get; set; }
+        public string namePrefix { get; set; }
+        public int startTime { get; set; }
+        public string place { get; set; }
+        public int regionId { get; set; }
+        public int[] tv { get; set; }
+        public bool liveHalf { get; set; }
+    }
+
+}
