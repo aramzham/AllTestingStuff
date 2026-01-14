79437f67109f86ddb1d31b7ee2c0f85cac5cd8a8
Aram.Zhamkochyan <aram.zhamkochyan@idt.am>
2026-01-14 14:10:41 +0400

    feat: added pipe operator

---
+++ Experimenting/PipeOperatorInCSharp/PipeOperator.cs
@@ -0,0 +1,7 @@
+﻿public static class PipeOperator
+{
+    extension<T, TResult>(T)
+    {
+        public static TResult operator |(T source, Func<T, TResult> func) => func(source);
+    }
+}
---
+++ Experimenting/PipeOperatorInCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var input = Console.ReadLine();
+var result = input
+             | int.Parse
+             | Math.Abs
+             | (x => x * 2);
+Console.WriteLine(result);
